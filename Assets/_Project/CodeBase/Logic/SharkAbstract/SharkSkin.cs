906b17828b7086973cfc75d0f4bc9cd07d6450d1
Нурлан Галиев <113087665+Nurlan1995G@users.noreply.github.com>
2024-06-13 12:59:29 +0400

    Shop

---
+++ Assets/_Project/CodeBase/Logic/SharkAbstract/SharkSkin.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class SharkSkin : MonoBehaviour
+{
+    public int Id { get; set; }
+}
--- Assets/_Project/CodeBase/Logic/SharkAbstract/SharkSkinManager.cs
+++ Assets/_Project/CodeBase/Logic/SharkAbstract/SharkSkinManager.cs
@@ -3,13 +3,24 @@ using UnityEngine;
 
 public class SharkSkinManager : MonoBehaviour
 {
-    [field: SerializeField] public List<GameObject> SkinsShark = new List<GameObject>();
+    [field: SerializeField] public List<SharkSkin> SkinsSharks = new List<SharkSkin>();
 
-    private GameObject _currentSkin;
+    private List<int> _idSharkSkin = new List<int> { 31620, 31482, 31482, 31408, 32128, 31596 };
+
+    private SharkSkin _currentSkin;
+
+    private void Awake()
+    {
+        for (int i = 0; i < SkinsSharks.Count; i++)
+        {
+            if(i < _idSharkSkin.Count)
+                SkinsSharks[i].Id = _idSharkSkin[i];
+        }
+    }
 
     public void ChangeSkinShark(int indexSkin)
     {
-        if (indexSkin < 0 || indexSkin >= SkinsShark.Count)
+        if (indexSkin < 0 || indexSkin >= SkinsSharks.Count)
         {
             Debug.LogError("Invalid skinShark index");
             return;
@@ -17,10 +28,10 @@ public class SharkSkinManager : MonoBehaviour
 
         if (_currentSkin != null)
         {
-            _currentSkin.SetActive(false);
+            _currentSkin.gameObject.SetActive(false);
         }
 
-        _currentSkin = SkinsShark[indexSkin];
-        _currentSkin.SetActive(true);
+        _currentSkin = SkinsSharks[indexSkin];
+        _currentSkin.gameObject.SetActive(true);
     }
 }

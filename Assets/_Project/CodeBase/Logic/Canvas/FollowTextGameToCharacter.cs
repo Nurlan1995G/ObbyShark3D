0247cb3818320073f2e6fc9cbcbe183baa66e4c9
Нурлан Галиев <113087665+Nurlan1995G@users.noreply.github.com>
2024-06-24 11:52:48 +0400

    CanvasShark

---
+++ Assets/_Project/CodeBase/Logic/Canvas/FollowTextGameToCharacter.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+namespace Assets._Project.CodeBase.Logic.Canvas
+{
+    public class FollowTextGameToCharacter : MonoBehaviour
+    {
+        [SerializeField] private Shark _characterObject;
+
+        private Vector3 _offset;
+
+        private void Start()
+        {
+            _offset = transform.position - _characterObject.transform.position;
+        }
+
+        private void Update()
+        {
+            Vector3 targetPosition = _characterObject.transform.position + _offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 50f);
+        }
+    }
+}

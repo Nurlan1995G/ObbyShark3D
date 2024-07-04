eeab55925d2f095d16dcaa7c8b1b4f90d4b917e1
Нурлан Галиев <113087665+Nurlan1995G@users.noreply.github.com>
2024-07-04 13:56:50 +0400

    FinalBuild

--- Assets/_Project/CodeBase/Player/UI/BoostButtonUI.cs
+++ Assets/_Project/CodeBase/Player/UI/BoostButtonUI.cs
@@ -1,11 +1,24 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Assets.Project.CodeBase.Player.UI
 {
     public class BoostButtonUI : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
+        [SerializeField] private TextMeshProUGUI _textShift;
+        [SerializeField] private TextMeshProUGUI _textBoost;
+        
         private PlayerMover _playerMover;
+
+        private void Start()
+        {
+            if (Application.isMobilePlatform)
+            {
+                _textShift.gameObject.SetActive(false);
+                _textBoost.gameObject.SetActive(true);
+            }
+        }
 
         public void Initialize(PlayerMover playerMover) =>
             _playerMover = playerMover;
